d30123745e55d446e583c50256b8b26028c6f762
Ramtn695 <ramaswamytnr@gmail.com>
2021-06-22 13:52:34 +0530

    My First React App and Published to Github Pages.

---
+++ src/Todo/App.js
@@ -0,0 +1,14 @@
+import Todo from "./Todo";
+import './Styles.css'
+
+function App(props) {
+    return (
+        <div className="App">
+            <h1>Your Favourite To_Do App </h1>
+            <h1>Your ToDo's Here</h1>
+            <Todo />
+        </div>
+    )
+}
+
+export default App;
